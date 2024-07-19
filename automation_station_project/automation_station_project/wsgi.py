661a5e3e991980df9d6d009a841748f7175518f7
chaddacus <chad.simon@cloud-warriors.com>
2024-07-19 12:31:31 -0400

    updated wsgi, startup and celery

--- automation_station_project/automation_station_project/wsgi.py
+++ automation_station_project/automation_station_project/wsgi.py
@@ -1,5 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automation_station.settings')
+
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automation_station_project.settings')
 application = get_wsgi_application()
