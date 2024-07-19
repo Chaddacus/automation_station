8e4da7e5fe49db297cbad3d700aa5757d9e3fe5c
chaddacus <chad.simon@cloud-warriors.com>
2024-07-18 22:43:47 -0400

    update to procfile and wsgi

--- automation_station_project/automation_station_project/wsgi.py
+++ automation_station_project/automation_station_project/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automation_station_project.automation_station_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automation_station_project.settings')
 application = get_wsgi_application()
