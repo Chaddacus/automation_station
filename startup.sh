84ec566944c9d9c34a790c40e74968575e3e41a2
chaddacus <chad.simon@cloud-warriors.com>
2024-07-19 13:29:54 -0400

    edited startup

--- startup.sh
+++ startup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Determine the project root directory dynamically
-PROJECT_ROOT=$(dirname $(dirname $(realpath $0)))
-
+#PROJECT_ROOT=$(dirname $(dirname $(realpath $0)))
+PROJECT_ROOT=$(pwd)
 # Export the PYTHONPATH
-export PYTHONPATH=$PROJECT_ROOT/automation_station_project
+echo "hideee ho!"
 
+export PYTHONPATH=$PROJECT_ROOT/automation_station_project
+echo "you smell flowery"
+echo $PYTHONPATH
 # Start Gunicorn
 exec gunicorn --bind=0.0.0.0:8000 --timeout 600 automation_station_project.wsgi:application
